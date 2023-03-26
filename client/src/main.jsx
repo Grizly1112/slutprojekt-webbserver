ae09be2590395f5c29e3b69630a938ab76f11d9a
Grizly1112 <81764115+Grizly1112@users.noreply.github.com>
2023-03-27 01:46:19 +0200

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import './index.css'
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home'
