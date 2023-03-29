d51a889db857732cb17857b4af749c12d734a16f
Grizly1112 <81764115+Grizly1112@users.noreply.github.com>
2023-03-30 01:08:57 +0200

    More fixes to online user widget

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useState } from 'react'
+import React, { memo, useEffect, useMemo, useRef, useState } from 'react'
 import { FaChartBar, FaChartLine, FaChartPie, FaCircleNotch, FaCriticalRole, FaHammer, FaJsSquare, FaMailBulk, FaRedhat, FaSearch, FaUsers } from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 import './css/Home.css'
@@ -14,7 +14,6 @@ export default function Home() {
   const [onlineUsers, setonlineUsers] = useState([]);
   const socket = useRef(null);
   const [hasLoadedInOnce,setHasLoadedInOnce] = useState(false)
-
 
   // Connect socket server
   useEffect(() => {
@@ -34,7 +33,7 @@ export default function Home() {
           throw err;
         });
     } else {
-      socket.current.emit("new-user-add", ("guest"));
+      socket.current.emit("new-user-add", (""));
     }
 
     socket.current.on("get-users", (users) => {
@@ -72,32 +71,49 @@ export default function Home() {
   }
 
   const OnlineUserWidget = () => {
+    const userOfEachCategory = useMemo(() => {
+      const count = { users: 0, guests: 0 };
+      onlineUsers.forEach((userOnline) => {
+        if (!userOnline.userId) {
+          count.guests++;
+        } else if (userOnline.userId !== undefined) {
+          count.users++;
+        }
+      });
+      return count;
+    }, [onlineUsers]);
+  
     return (
       <SideWidget icon={<FaUsers />} title="Användare online" live={true}>
         <>
-          <UsersOnline onlineUsers={onlineUsers} />
+          <UsersOnline onlineUsers={onlineUsers} userOfEachCategory={userOfEachCategory} />
           <hr />
-          <h5 className='totalOnlineUSerLabel'>Totalt {onlineUsers.length} stycken (Medlemmar: 4, Gäster: 3)</h5>
-          <NavLink to="#" className="viewmore"><FaSearch /> Visa alla användare online</NavLink>
+          <h5 className="totalOnlineUSerLabel">
+            Totalt {onlineUsers.length} stycken (Medlemmar: {userOfEachCategory.users}, Gäster: {userOfEachCategory.guests})
+          </h5>
+          <NavLink to="#" className="viewmore">
+            <FaSearch /> Visa alla användare online
+          </NavLink>
         </>
       </SideWidget>
-    )
-  }
-  // Memo will memorize the UserOnline state and only rerender whne onlineUser upate and therefore prevent unnecessary rerendering
-  const UsersOnline = memo(({ onlineUsers }) => {
+    );
+  };
+  
+  const UsersOnline = memo(({ onlineUsers, userOfEachCategory }) => {
     return (
-      <div className='usersOnline'>
+      <div className="usersOnline">
         {onlineUsers.map((userOnline) => {
-          if(userOnline.userId !== 'guest' && userOnline.userId !== undefined) {
-            // const { _id, img } = userOnline.userI;
+          if (userOnline.userId !== "guest" && userOnline.userId !== undefined) {
             return (
-              <Tooltip label={userOnline.userId}>
-                <img src={userOnline.pfp || userDefault} className="onlineUsers-img" alt="" />
-              </Tooltip>
+              <NavLink to={`user/${userOnline.userId}`}>
+                <Tooltip key={userOnline.userId} label={userOnline.userId}>
+                  <img src={userOnline.pfp || userDefault} className="onlineUsers-img" alt="" />
+                </Tooltip>
+              </NavLink>
             );
           }
         })}
-        {onlineUsers.length > 4 && <h4>{onlineUsers.length - 4}+ andra</h4>}
+        {userOfEachCategory.users > 4 && <h4>{userOfEachCategory.users - 4}+ andra</h4>}
       </div>
     );
   });
